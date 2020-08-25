0f6c5af5891a441b5b99a0ba9b3b87a276ca47c1
Valerio Fusco <valeriofusco1109v@gmail.com>
2020-08-25 18:20:48 +0200

    Fix

--- server.js
+++ server.js
@@ -3,15 +3,13 @@ const server = express();
 const PORT = process.env.PORT||5000;
 const emailRoute = require('./api/routes/email');
 const mongoose = require('mongoose')
-const bodyParser = require('body-parser')
 server.use(express.json())
+
+server.use('/email',emailRoute)
 mongoose.set('useFindAndModify', false);
 const uri = "mongodb+srv://valeffe1109:Vergine97@cluster0.zv9ag.mongodb.net/Cluster0?retryWrites=true&w=majority";
-server.use('/email',emailRoute)
-
 
 mongoose.connect(uri , { useNewUrlParser: true,useUnifiedTopology: true })  
-
 
 server.listen(PORT,()=>{
     console.log(`Server is running on port ${PORT}`)
