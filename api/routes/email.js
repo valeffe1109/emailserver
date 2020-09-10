77d29fa600499f64a549622062b4cb3d3032ecb6
Valerio Fusco <valeriofusco1109v@gmail.com>
2020-09-10 13:07:48 +0200

    VARIOUS FIXES  ADDED SENDGRID

--- api/routes/email.js
+++ api/routes/email.js
@@ -1,56 +1,57 @@
 const express = require("express");
 const router = express.Router();
 const nodemailer = require("nodemailer");
-const Customer = require('../../models/Customer');
+const Customer = require("../../models/Customer");
 
 const transport = nodemailer.createTransport({
-  service: "gmail",
+  service: "sendgrid",
   auth: {
-    user: "valeriofuscotest@gmail.com",
-    pass: "Vergine97",
+    api_user: "allelica",
+    api_key: "0Nmdcdnv7!_sendgrid",
   },
 });
 
-router.post("/",async(req, res) => {
-    const message = {
-      from: "elonmusk@tesla.com",
-      to: `${req.body.email}`,
-      subject: "Request a demo with Allelica",
-      html: `<p>Dear ${req.body.name},<br>
+router.post("/", async (req, res) => {
+  const message = {
+    from: "info@allelica.com",
+    to: `${req.body.email}`,
+    subject: "Request a demo with Allelica",
+    html: `<p>Dear ${req.body.name},<br>
     
-            Thank you for your interest in Allelica’s Polygenic Risk Score Software.<br>
-            
-            We are glad to welcome you to Allelica’s community of medical professionals and researchers.<br>
-            
-            One of our product specialists will be in touch with you shortly.<br> 
-            
-            In the meantime, you are welcome to book your free demo directly using this link: https://calendly.com/allelica/demo<br>
-            
-            Best regards,<br>
-            
-            Allelica team
+      Thank you for visiting our website! We are glad to welcome you to Allelica’s community of medical professionals and researchers. <br/>
+
+      To discover more about polygenic risk scores for complex diseases or to explore the various functionalities of our technology, you can visit our blog here. <br/>
+      
+      You can also book a free demo of our software with one of Allelica’s specialists today: https://calendly.com/allelica/demo <br/>
+      
+      If you can’t find a time that suits you, just let me know and I’d be happy to accommodate your schedule.<br/>
+      
+      For any questions you may have, feel free to send me an email. We look forward to connecting with you. <br/>
+      
+      Have a lovely day, <br/>
+      
+      Colleen <br/>
             </p>`,
-    };
+  };
 
-    const customer = new Customer({
-      name: req.body.name,
-      email:req.body.email
-    })
-    try {
-      const savedCustomer = await customer.save();
-      res.send(savedCustomer);
-    } catch (err) {
-      res.status(400).send(err);
+  const customer = new Customer({
+    name: req.body.name,
+    email: req.body.email,
+  });
+  try {
+    const savedCustomer = await customer.save();
+    res.send(savedCustomer);
+  } catch (err) {
+    res.status(400).send(err);
+  }
+
+  transport.sendMail(message, function (err, info) {
+    if (err) {
+      res.send(err);
+    } else {
+      res.send("Success");
     }
-
-    transport.sendMail(message, function (err, info) {
-      if (err) {
-        res.send(err)
-      } else {
-        res.send('Success')
-      }
-    });
-    
+  });
 });
 
 module.exports = router;
